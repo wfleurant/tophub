1b6648503f5a1e326c984ef42ddd70a875c58d42
Igel Broski <igel@hyperboria.ca>
2015-02-14 22:42:36 -0500

    hubeventlog: cleanup

--- include/HubEventLog.php
+++ include/HubEventLog.php
@@ -5,28 +5,13 @@ use Monolog\Handler\StreamHandler;
 
 function yell($type=false,$string=false,$bulletlist=null)
 {
-
-    if(is_array($string)){ print("\n");
-        $string=var_dump($string);
+    if((is_array($string)) || is_object($string)){
+        print_r($string);
+        return;
+    } else {
         echo ColorCLI::getColoredString($string, 'white');
         print("\n");
-        return;
     }
-
-    if($type == "monolog") {
-
-        // $log = new Logger('name');
-        // $log->pushHandler(new StreamHandler('./log_ioStats.log', Logger::WARNING));
-
-        // $log->addWarning('Foo');
-        // $log->addError("$string");
-
-    }
-    // if ($type == "error") {
-        echo ColorCLI::getColoredString($string, 'white');
-        print("\n");
-        return;
-    // }
 }
 
 ?>
