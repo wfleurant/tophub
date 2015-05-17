2e8038cc73dbeef432fcf1a59b9e7aa951615f20
Igel Broski <igel@hyperboria.ca>
2015-05-17 00:41:48 -0400

    repo: tophub v12-v16

--- tophub-v12-v16/include/HubConfig.php
+++ tophub-v12-v16/include/HubConfig.php
@@ -3,6 +3,7 @@ $tophub_revi = '1.0';
 $tophub_port = 1617;
 $tophub_host = 'fccc:5b2c:2336:fd59:794d:c0fa:817d:8d8';
 $tophub_host = '127.0.0.1';
+$tophub_host = 'fc00::1';
 $tophub_sums = 0;
 
 $tophub_head = tophub_headers($tophub_revi);
--- tophub-v12-v16/include/db/HubMySQL.php
+++ tophub-v12-v16/include/db/HubMySQL.php
@@ -2,7 +2,7 @@
 $loop_mysql = React\EventLoop\Factory::create();
 $connection = new React\MySQL\Connection($loop_mysql, array(
     'dbname' => 'tophub',
-    'user'   => 'tophub',
-    'passwd' => 'tophub'
+    'user'   => 'root',
+    'passwd' => 'DRxzmv5DZFB60ziPFnwg',
 ));
 ?>
