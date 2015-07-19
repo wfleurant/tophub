38e13bdddaa4b3890eb367d0807ce23856699ef5
Igel Broski <igel@hyperboria.ca>
2015-07-19 01:32:22 -0400

    app: cleanup

--- tophub/bootstrap/app.php
+++ tophub/bootstrap/app.php
@@ -4,19 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 Dotenv::load(__DIR__.'/../');
 
-/*
-| Create The Application
-*/
+/* Create The Application */
 
 $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
 
-/*
-| Register Container Bindings
+/* Register Container Bindings
 	$app->withFacades();
 */
+
 $app->register('Jenssegers\Mongodb\MongodbServiceProvider');
 $app->withEloquent();
 
@@ -30,31 +28,28 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
-/*
-| Register Middleware
+
+/* Register Middleware
 	$app->middleware([
 		Illuminate\Cookie\Middleware\EncryptCookies::class,
 		Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
 		Illuminate\Session\Middleware\StartSession::class,
 		Illuminate\View\Middleware\ShareErrorsFromSession::class,
 		Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
+	]);
+	
 	$app->routeMiddleware([
 
 	]);
 */
-]);
 
-
-/*
-| Register Service Providers
+/* Register Service Providers
 	$app->register(App\Providers\AppServiceProvider::class);
 	$app->register(App\Providers\EventServiceProvider::class);
 */
 
 
-/*
-| Load The Application Routes
-*/
+/* Load The Application Routes */
 
 $app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
     require __DIR__.'/../app/Http/routes.php';
